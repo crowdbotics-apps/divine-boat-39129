13db0d97e71a0f7fd56ec450a76ed9770d9c6567
Crowdbotics <team@crowdbotics.com>
2023-02-27 13:40:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-boat-39129",
+  url: "https://divine-boat-39129.botics.co",
+  api: "https://divine-boat-39129.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
